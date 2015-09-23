09ec2e05d9cab289aed7fe339734ddd62625f8d1
31316972 <31316972@092946-P31S401.macklabs.br>
2015-09-23 11:59:50 -0300

    Troquei um for de lugar pois fazia mais sentido na classe papel moeda

--- Source Code Inspection/src/br/calebe/ticketmachine/core/PapelMoeda.java
+++ Source Code Inspection/src/br/calebe/ticketmachine/core/PapelMoeda.java
@@ -1,13 +1,18 @@
 package br.calebe.ticketmachine.core;
 
+import br.calebe.ticketmachine.exception.PapelMoedaInvalidaException;
+import java.util.Iterator;
+
 /**
  *
  * @author Calebe de Paula Bianchini
  */
 public class PapelMoeda {
 
+    protected int saldo;
     protected int valor;
     protected int quantidade;
+    protected int[] papelMoeda = {2, 5, 10, 20, 50, 100};
 
     public PapelMoeda(int valor, int quantidade) {
         this.valor = valor;
@@ -20,5 +25,26 @@ public class PapelMoeda {
 
     public int getQuantidade() {
         return quantidade;
+    }
+
+    public void inserir(int quantia) throws PapelMoedaInvalidaException {
+        boolean achou = false;
+        for (int i = 0; i < papelMoeda.length && !achou; i++) {
+            if (papelMoeda[i] == quantia) {
+                achou = true;
+            }
+        }
+        if (!achou) {
+            throw new PapelMoedaInvalidaException();
+        }
+        this.saldo += quantia;
+    }
+
+    public int getSaldo() {
+        return saldo;
+    }
+
+    public Iterator<Integer> getTroco() {
+        return null;
     }
 }
